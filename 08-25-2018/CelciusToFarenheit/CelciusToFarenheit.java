74bcf6fd625d68f9f463fd10b04e4547bc64eeb2
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-09-07 21:24:27 -0700

    change the 1.8 to use 9/5 and also change the description of CelciusToFarenheit.java

--- 08-25-2018/CelciusToFarenheit/CelciusToFarenheit.java
+++ 08-25-2018/CelciusToFarenheit/CelciusToFarenheit.java
@@ -1,22 +1,22 @@
 //Name: Jaime Garcia Garcia
 //Date: 08-25-2018
-//Application: Circumference of a Circle
-//Purpose: The Application use escape character to print the following message
+//Application: Celcius To Farenheit
+//Purpose: The application convert celcius to farenheit 
 
 class CelciusToFarenheit {
     public static void main(String[] args) {
         //Number Values
-        int celsiusValue = 31;
-        final double onePointEight  = 1.8;
+        int celsiusValue = 50;
+        final float nineOverEight  = 9/5F;
         final int thirtyTwo = 32;
-        double celciusFarenheitToValue = celsiusValue * onePointEight + thirtyTwo;
+        float celciusFarenheitToValue = celsiusValue * nineOverEight + thirtyTwo;
 
         //Special Characters
         char degreeSymbol = '\u00B0';
 
         //Messages
-        String celsiusMessage = Double.toString(celsiusValue) + degreeSymbol + "C";
-        String farenheitToCelciusMessage = Double.toString(celciusFarenheitToValue) + degreeSymbol + "F";
+        String celsiusMessage = Integer.toString(celsiusValue) + degreeSymbol + "C";
+        String farenheitToCelciusMessage = Float.toString(celciusFarenheitToValue) + degreeSymbol + "F";
         
         //Print Statements
         System.out.println(celsiusMessage + " is " + farenheitToCelciusMessage);
