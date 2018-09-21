a1e5c7c78c81c9944df0908e64a6a7b936241dc0
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-09-20 23:37:29 -0700

    wrote methods for getCircumferenceOfCircle, getCelsiusToFahrenheit, getFahrenheitToCelsius, and getQuadraticFormula

--- 09-15-2018/MethodsCollection/MethodsCollection.java
+++ 09-15-2018/MethodsCollection/MethodsCollection.java
@@ -1,12 +1,37 @@
 class MethodsCollection {
     static double getAreaOfCircle(double radius) {
-        double areaOfCircle = Math.pow(radius, 2) * Math.PI;
+        double areaOfCircle = Math.PI * Math.pow(radius, 2);
         return areaOfCircle;
     }
 
+    static double getCircumferenceOfCircle(double radius) {
+        double circumferenceOfCircle = 2 * Math.PI * radius;
+        return circumferenceOfCircle;
+    }
+
+    static double getCelsiusToFahrenheit(double celsius) {
+        double celsiusToFahrenheit = ((9 * celsius)/5) + 32;
+        return celsiusToFahrenheit;
+    }
+
+    static double getFahrenheitToCelsius(double farhrenheit) {
+        double fahrenheitToCelsius = ((farhrenheit - 32) * 5)/9;
+        return fahrenheitToCelsius;
+    }
+
+    static double getQuadraticFormula(double squareCoefficient, double singleCofficient, double constantNumber) {
+        double quadraticFormulaPositiveOutput = (-(singleCofficient) + Math.sqrt(Math.pow(singleCofficient,2) - (4 * squareCoefficient * constantNumber)))/(2 * squareCoefficient);
+        return quadraticFormulaPositiveOutput;
+    }
+
     public static void main(String[] args) {
-        
         double CircleInput = getAreaOfCircle(20);
+        double celsiusInput = getCelsiusToFahrenheit(14);
+        double farhrenheitInput = getFahrenheitToCelsius(6);
+        double quadraticInput = getQuadraticFormula(5,3,9);
         System.out.println(CircleInput);
+        System.out.println(celsiusInput);
+        System.out.println(farhrenheitInput);
+        System.out.println(quadraticInput);
     }
 }
