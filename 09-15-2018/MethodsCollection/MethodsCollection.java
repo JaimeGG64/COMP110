c697c43ab52d3f55e6bfd05e77f42bc6204f7c4a
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-09-22 01:50:54 -0700

    import code from desktop computer to conserve laptop battery

--- 09-15-2018/MethodsCollection/MethodsCollection.java
+++ 09-15-2018/MethodsCollection/MethodsCollection.java
@@ -1,3 +1,7 @@
+//Name: Jaime Garcia Garcia
+//Date: 09-15-2018
+//Application: Collection of Methods
+//Purpose: The application has a series of a methods that calculate the area/circumference of a circle, convert beteen fahrenheit and celsius, and has the quadatic formula
 class MethodsCollection {
     static double getAreaOfCircle(double radius) {
         double areaOfCircle = Math.PI * Math.pow(radius, 2);
@@ -25,13 +29,35 @@ class MethodsCollection {
     }
 
     public static void main(String[] args) {
-        double CircleInput = getAreaOfCircle(20);
-        double celsiusInput = getCelsiusToFahrenheit(14);
-        double farhrenheitInput = getFahrenheitToCelsius(6);
-        double quadraticInput = getQuadraticFormula(5,3,9);
-        System.out.println(CircleInput);
-        System.out.println(celsiusInput);
-        System.out.println(farhrenheitInput);
-        System.out.println(quadraticInput);
+        //Area of Circle
+        double regularCircleInputArea = getAreaOfCircle(20);
+        double negativeCircleInputArea = getAreaOfCircle(-5);
+        double zeroCircleInputArea = getAreaOfCircle(0);
+        String areaOfCircleMessage = "The Area of Circle Method gave the follow output \n" + Double.toString(regularCircleInputArea) + "\n" + Double.toString(negativeCircleInputArea) + "\n" + Double.toString(zeroCircleInputArea);
+
+        //Circumference of Circle
+        double regularCircleInputCircumference = getCircumferenceOfCircle(2);
+        double pointCircleInputCircumference = getCircumferenceOfCircle(2.5);
+        double zeroCircleInputCircumference = getCircumferenceOfCircle(0);
+        String CircumferenceOfCircleMessage = "The Circumference of Circle Method gave the follow output \n" + Double.toString(regularCircleInputCircumference) + "\n" + Double.toString(pointCircleInputCircumference) + "\n" + Double.toString(zeroCircleInputCircumference);
+
+        //Celsius to Fahrenheit
+        double regularTempetureInputFahrenheit = getCelsiusToFahrenheit(6);
+        double zeroTempetureInputFahrenheit = getCelsiusToFahrenheit(0);
+        double negativeTempetureInputFahrenheit = getCelsiusToFahrenheit(-8);
+        String CelsiusToFahrenheitMessage = "The Celsius to Fahrenheit Method gave the follow output \n" + Double.toString(regularTempetureInputFahrenheit) + "\n" + Double.toString(zeroTempetureInputFahrenheit) + "\n" + Double.toString(negativeTempetureInputFahrenheit);
+
+        //Fahrenheit to Celsius
+        double regularTempetureInputCelsius = getFahrenheitToCelsius(7);
+        double zeroTempetureInputCelsius = getFahrenheitToCelsius(0);
+        double pointTempetureInputCelsius = getFahrenheitToCelsius(9.4);
+        String FahrenheitToCelsiusMessage = "The Fahrenheit to Celsius Method gave the follow output \n" + Double.toString(regularTempetureInputCelsius) + "\n" + Double.toString(zeroTempetureInputCelsius) + "\n" + Double.toString(pointTempetureInputCelsius);
+
+        //Outputs
+        System.out.println(areaOfCircleMessage);
+        System.out.println(CircumferenceOfCircleMessage);
+        System.out.println(CelsiusToFahrenheitMessage);
+        System.out.println(FahrenheitToCelsiusMessage);
+        System.out.println();
     }
 }
--- 09-15-2018/Selection/Selection.java
+++ 09-15-2018/Selection/Selection.java
@@ -1,4 +1,63 @@
+//Name: Jaime Garcia Garcia
+//Date: 09-15-2018
+//Application: Collection of Methods
+//Purpose: The application has a series of a methods that calculate the area/circumference of a circle, convert beteen fahrenheit and celsius, and has the quadatic formula
 class Selection {
+    //A - If Statment
+    public static void AifCheckExample(boolean hasCard, double money) {
+        if(money > 4 || hasCard == true) {
+            System.out.println("Enjoy your meal.");
+        }
+    }
+
+    //B - If, Else Statement
+    public static void BifElseCheckExample(boolean hasDriverLicense, int age) {
+        if(hasDriverLicense == true && age >= 16){
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("You cannot drive");
+        }
+    }
+
+    //C - If, Else If, Else Statment
+    // public static void CifElseIfElseCheckExample(boolean inDebt, double balance) {
+    //     if (inDebt != false){
+    //         double balance = -balance;
+    //     }
+    //     else if (inDebt != true) {
+    //         double balance = -balance;
+    //     }
+    //     else {
+    //         System.out.print("What happened to your money");
+    //     }
+    // }
+
+    //D - If, Else If, Else If, Else
+    public static void DifElseIfElseIfElseCheckExample(int pokedexNumber) {
+        if (pokedexNumber < 386){
+            System.out.println("Pokemon can be found in any region");
+        }
+        else if (pokedexNumber < 251) {
+            System.out.print("Pokemon can be found in Kanto and Johto");
+        }
+        else if (pokedexNumber <= 151) {
+            System.out.print("Pokemon can only be found in Kanto");
+        }
+        else {
+            System.out.print("Pokemon might be nonexistent");
+        }
+    }
+
+    //E - Nested If
+    // public static void ENestedIfExample() {
+    //     if(username.equalTo("Jim101")) {
+    //         if(password.equalTo("password1")) {
+    //             System.out.println("Welcome");
+    //         }    
+    //     }
+    // }
+
     public static boolean selectionA(String s) {
         boolean result = false;
         if(s.length() > 5) {
@@ -8,13 +67,16 @@ class Selection {
         return result;
     }
 
-    public static void selection(int age) {
-        switch(age) {
-            case 1 : System.out.println("baby");
+    //F - Switch Case
+    public static void switchCaseExample(int year) {
+        switch(year) {
+            case 1 : System.out.println("Beginning of Common Era");
                 break;
-            case 13 : System.out.println("teen");
+            case 2000 : System.out.println("Y2K");
                 break;
-            case 16 : System.out.println("can drive");
+            case 2018 : System.out.println("Current Year");
+                break;
+            case 3000 : System.out.println("The Future");
                 break;
             default : System.out.println("Does not support");
         }
@@ -25,8 +87,11 @@ class Selection {
         boolean val = selectionA(word);
         System.out.println("Is " + word + " more than 5 characters? " + val);
         
-        selection(20);
-        selection(1);
-        selection(16);
+        AifCheckExample(true, 20);
+        BifElseCheckExample(false, 15);
+        // CifElseIfElseCheckExample(false, 2);
+        DifElseIfElseIfElseCheckExample(345);
+
+        switchCaseExample(1);
     }
 }
