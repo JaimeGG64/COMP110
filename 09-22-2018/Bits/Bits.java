00fe7b2017dd414d60b8c06227dcc02a07c5ec17
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-09-29 13:00:33 -0700

    add binary descriptions on the bits

--- 09-22-2018/Bits/Bits.java
+++ 09-22-2018/Bits/Bits.java
@@ -5,7 +5,10 @@ public class Bits {
     // Bitwise and Bitshift Operators
 
     /* The & Operator (Bitwise AND)
-    
+        10100 (20)
+       &11001 (25)
+        -----
+        10000 (16)
     */
     int intAndOne = 20;
     int intAndTwo = 25;
@@ -14,7 +17,10 @@ public class Bits {
     JOptionPane.showMessageDialog(null, Integer.toString(intAndOne) + " & " + Integer.toString(intAndTwo) + " = " + Integer.toString(resultOneAnd) + "\n" + resultOneAndStr);
 
     /* The | Operator (Bitwise Inclusive OR)
-    
+        10100 (10)
+       |11110 (30)
+        -----
+        11110 (30)
     */
     int intIncOrOne = 10;
     int intIncOrTwo = 30;
@@ -23,7 +29,10 @@ public class Bits {
     JOptionPane.showMessageDialog(null, Integer.toString(intIncOrOne) + " | " + Integer.toString(intIncOrTwo) + " = " + Integer.toString(resultOneIncOr) + "\n" + resultOneIncOrStr);
 
     /* The ^ Operator (Bitwise Exclusive OR)
-    
+        01110 (14)
+       ^10100 (20)
+        -----
+        11010 (30)
     */
     int intExcOrOne = 14;
     int intExcOrTwo = 20;
@@ -32,9 +41,11 @@ public class Bits {
     JOptionPane.showMessageDialog(null, Integer.toString(intExcOrOne) + " ^ " + Integer.toString(intExcOrTwo) + " = " + Integer.toString(resultOneExcOr) + "\n" + resultOneExcOrStr);
 
     /* The ~ Operator (Bitwise Inverts ~)
-    
+        ~(1001010)
+        -----------
+        111111111111111111001010
     */
-    int intInvOne = 75;
+    int intInvOne = 74;
     int resultInv = ~(intInvOne);
     String resultInvStr = String.format("%32s", Integer.toBinaryString(resultInv)).replace(' ', '0');
     JOptionPane.showMessageDialog(null, "~ " + Integer.toString(intInvOne) + " = " + Integer.toString(resultInv) + "\n" + resultInvStr);
@@ -44,7 +55,7 @@ public class Bits {
     
     */
     // int intRightShift = 56;
-    // int resultRightShift = ~(intRightShift >> 4);
-    // String resultRightShiftStr = String.format("%32s", Integer.toBinaryString(resultRightShift)).replace(' ', '0');
-    // JOptionPane.showMessageDialog(null, ">> " + Integer.toString(intRightShift) + " = " + Integer.toString(resultRightShift) + "\n" + resultRightShiftStr);
+    // int resultRightShift = intRightShift >> 4;
+    // String resultRightShiftStr = String.format(Integer.toBinaryString(resultRightShift));
+    // System.out.println( ">> " + intRightShift + " = " + resultRightShiftStr + "\n");
 }
