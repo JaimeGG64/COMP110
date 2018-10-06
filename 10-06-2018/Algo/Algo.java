6f13bcbc393deff0796db2eb7213bd0d80802eb8
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-06 12:03:28 -0700

    commit blank java files to know which project must be completed

---
+++ 10-06-2018/Algo/Algo.java
@@ -0,0 +1,10 @@
+/*
+min
+max
+odd
+even 
+palindrome 
+find if char exists in a string (returns a boolean)
+All methods discussed in class (Slide #3)
+Call each method 3 times from `main` providing different arguments each time
+*/
---
+++ 10-06-2018/Jiminy/Jiminy.java
@@ -0,0 +1,26 @@
+/*
+How should you design your app?
+If the user doesn’t enter an option (aka. argument), then print a menu showing the available options for your app
+How do I know the user didn’t provide an option?
+HINT: args.length == 0
+Start of with 2 options: “-date” & “-time”
+Use switch case statement to see which option was given by the user
+How do I run/test my app?
+Execute the following in the terminal: 
+“java Jiminy”
+“java Jiminy -date”
+“java Jiminy -time”
+“java Jiminy -date -time”
+
+Jiminy must support the following options
+-date
+-time
+-proc
+Available processors
+-freemem
+Free memory
+-maxmem
+Maximum memory
+-totmem
+Total Memory
+*/
