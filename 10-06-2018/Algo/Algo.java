75c35c96fc1bc216a767176e26a4dca9c8b5973f
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-06 13:11:19 -0700

    Algo getMinValue, getMaxValue, isOdd, and isEven methods are functional

--- 10-06-2018/Algo/Algo.java
+++ 10-06-2018/Algo/Algo.java
@@ -1,10 +1,54 @@
 /*
-min
-max
-odd
-even 
-palindrome 
-find if char exists in a string (returns a boolean)
-All methods discussed in class (Slide #3)
-Call each method 3 times from `main` providing different arguments each time
+-min
+-max
+-odd
+-even 
+-palindrome 
+-find if char exists in a string (returns a boolean)
+-All methods discussed in class (Slide #3)
+-Call each method 3 times from `main` providing different arguments each time
 */
+//Name: Jaime Garcia Garcia
+//Date: 10-06-2018
+//Application: Algo
+//Purpose: Using methods
+class Algo{
+    public static int getMinValue(int input1, int input2){
+        if(input1 < input2){
+            return input1;
+        }
+        return input2;
+    }
+
+    public static int getMaxValue(int input1, int input2){
+        if(input1 > input2){
+            return input1;
+        }
+        return input2;
+    }
+
+    public static boolean isOdd(int input){
+        if(input % 2 != 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEven(int input){
+        if(input % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPalindrome(String Word){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int agecheck = getMinValue(3,4);
+        boolean oddcheck = isOdd(77);
+        System.out.println(agecheck);
+        System.out.println(oddcheck);
+    }
+}
