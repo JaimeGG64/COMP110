ea93af3bbbd24ee12a9a0688cb16667223f51c7e
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-13 13:32:05 -0700

    Arrays.java is complete

--- 10-06-2018/Arrays/Arrays.java
+++ 10-06-2018/Arrays/Arrays.java
@@ -9,27 +9,27 @@ Call each method 3 times from main (provide different arguments each time)
 //Application: Arrays
 //Purpose: Using methods and Arrays
 class Arrays{
-    public static int sumOfArray(int[] a){     
+    public static int sumOfArray(int[] arrayOfInteger){     
         int sum = 0;
-        for(int i = 0; i < a.length; i++){
-            sum += a[i];
+        for(int i = 0; i < arrayOfInteger.length; i++){
+            sum += arrayOfInteger[i];
         }
         return sum;
     }
 
-    public static int maxValueInArray(int[] a){
+    public static int maxValueInArray(int[] arrayOfInteger){
         int max = 0;
-        for(int i = 1; i < a.length; i++){
-            if (a[i] > max){
-                max = a[i]; 
+        for(int i = 1; i < arrayOfInteger.length; i++){
+            if (arrayOfInteger[i] > max){
+                max = arrayOfInteger[i]; 
             }
         }
         return max;
     }
 
-    public static boolean isStringInArray(String wordtoFind, String[] stringArray){
-        for(int i = 0; i < stringArray.length; i++){
-            if(stringArray[i].equals(wordtoFind)){
+    public static boolean isStringInArray(String wordToFind, String[] arrayOfString){
+        for(int i = 0; i < arrayOfString.length; i++){
+            if(arrayOfString[i].equals(wordToFind)){
               return true;  
             } 
         }
@@ -37,22 +37,28 @@ class Arrays{
     }    
 
     public static void main(String[] args){
-        int[] intArray = new int[6];
-        intArray[0] = 0;
-        intArray[1] = 2;
-        intArray[2] = 54;
-        intArray[3] = 22;
-        intArray[4] = 125;
-        intArray[5] = 8;
+        //Integer Arrays
+        int[] randomNumbers = new int[6];
+        randomNumbers[0] = 0;
+        randomNumbers[1] = 2;
+        randomNumbers[2] = 54;
+        randomNumbers[3] = 22;
+        randomNumbers[4] = -125;
+        randomNumbers[5] = 8;
 
-        int[] someNumbers = {
-            34,
-            3425,
-            254,
-            252,
-            342
+        int[] fibonacciNumbers = {
+            1,
+            2,
+            3,
+            5,
+            8,
+            13,
+            21
         };
 
+        int[] eightBitNumbers = {0,1,2,4,16,32,64,128,256};
+
+        //String Arrays
         String[] pets = new String[3];
         pets[0] = "dog";
         pets[1] = "cat";
@@ -66,20 +72,25 @@ class Arrays{
             "ham"
         };
 
-        boolean wordcheck = isStringInArray("bird", pets);
-        System.out.println(wordcheck);
+        String[] SouthParkCharacters = {"Kenny", "Stan", "Kyle", "Cartman" ,"Butters", "Wendy", "Token", "Mr. Garison"};
 
-        System.out.println(intArray[3]);
-        System.out.println(shoppingList[4]);
+        int sumRandomNumbers = sumOfArray(randomNumbers);
+        int sumFibonacciNumbers = sumOfArray(fibonacciNumbers);
+        int sumEightBitNumbers = sumOfArray(eightBitNumbers);
+        String sumOfArrayMessage = "The sumOfArray() method Gave the following outputs\n" + sumRandomNumbers + "\n" + sumFibonacciNumbers + "\n" + sumEightBitNumbers;
 
-        System.out.println(pets[1]);
-        System.out.println(someNumbers[2]);
+        int maxRandomNumbers = maxValueInArray(randomNumbers);
+        int maxFibonacciNumbers = maxValueInArray(fibonacciNumbers);
+        int maxEightBitNumbers = maxValueInArray(eightBitNumbers);
+        String maxValueInArrayMessage = "The maxValueInArray() method Gave the following outputs\n" + maxRandomNumbers + "\n" + maxFibonacciNumbers + "\n" + maxEightBitNumbers;
 
-        int sumnum = sumOfArray(intArray);
+        boolean checkPets = isStringInArray("cats", pets);
+        boolean checkShoppingList = isStringInArray("milk", shoppingList);
+        boolean checkSouthParkCharacters = isStringInArray("Fry", SouthParkCharacters);
+        String isStringInArrayMessage = "The isStringInArray() method Gave the following outputs\n" + checkPets + "\n" + checkShoppingList + "\n" + checkSouthParkCharacters;
 
-        int maxInt = maxValueInArray(intArray);
-
-        System.out.println(sumnum);
-        System.out.println(maxInt);
+        System.out.println(sumOfArrayMessage);
+        System.out.println(maxValueInArrayMessage);
+        System.out.println(isStringInArrayMessage);
     }
 }
