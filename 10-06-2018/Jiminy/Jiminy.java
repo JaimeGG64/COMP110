4564853fbd6a953702c4df57b5c4e712d49f5ad5
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-17 00:06:49 -0700

    addjust the wording when user select an option in Jiminy

--- 10-06-2018/Jiminy/Jiminy.java
+++ 10-06-2018/Jiminy/Jiminy.java
@@ -1,29 +1,3 @@
-/*
-Jiminy
-- Assistant app called Jiminy
-- App can be used in the terminal (like: date, time, pwd commands in Linux)
-- Provide user info (output on the terminal) depending on the arguments given in the command line
-- Make use of “String[] args” of the “main” method
-- **How should you design your app?**
-- If the user doesn’t enter an option (aka. argument), then print a menu showing the available options for your app
-- How do I know the user didn’t provide an option?
-- HINT: args.length == 0
-- Start of with 2 options: “-date” & “-time”
-- Use switch case statement to see which option was given by the user
-- How do I run/test my app?
-- Execute the following in the terminal: 
-    “java Jiminy”
-    “java Jiminy -date”
-    “java Jiminy -time”
-    “java Jiminy -date -time”
-- Jiminy must support the following options
-    -date - Current date
-    -time - Current time
-    -proc - Available processors
-    -freemem - Free memory
-    -maxmem- Maximum memory
-    -totmem -Total Memory
-*/
 //Name: Jaime Garcia Garcia
 //Date: 10-06-2018
 //Application: Jiminy
@@ -61,16 +35,16 @@ public class Jiminy{
                 case "-date": 
                     SimpleDateFormat todayDate;
                     todayDate = new SimpleDateFormat("MM-dd-yyyy");
-                    System.out.println(todayDate.format(getCurrentDateAndTime));
+                    System.out.println("Date: " + todayDate.format(getCurrentDateAndTime));
                     break;
                 case "-time":
                     SimpleDateFormat currentTime;
                     currentTime = new SimpleDateFormat("kk:mm:ss");
-                    System.out.println(currentTime.format(getCurrentDateAndTime));
+                    System.out.println("Time: " + currentTime.format(getCurrentDateAndTime));
                     break;
                 case "-proc":
-                    int processors = Runtime.getRuntime().availableProcessors();
-                    System.out.println("CPU cores: " + processors);
+                    int numAvailableProcessors = Runtime.getRuntime().availableProcessors();
+                    System.out.println("Available Processors: " + numAvailableProcessors);
                     break;
                 case "-freemem":
                     long freeMemory = Runtime.getRuntime().freeMemory();
@@ -86,6 +60,7 @@ public class Jiminy{
                     break;
                 case "-v":
                     System.out.print("version 1.0");
+                    break;
                 default: System.out.println(jiminyCompleteGreeting);
             }
             i++;
