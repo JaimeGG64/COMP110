b973908a0bc068e7eafc62151dfa49e242780f07
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-17 00:22:29 -0700

    remove assignment requirement for Algo.java and Arrays.java

--- 10-06-2018/Algo/Algo.java
+++ 10-06-2018/Algo/Algo.java
@@ -1,13 +1,3 @@
-/*
--min
--max
--odd
--even 
--palindrome 
--find if char exists in a string (returns a boolean)
--All methods discussed in class (Slide #3)
--Call each method 3 times from `main` providing different arguments each time
-*/
 //Name: Jaime Garcia Garcia
 //Date: 10-06-2018
 //Application: Algo
--- 10-06-2018/Arrays/Arrays.java
+++ 10-06-2018/Arrays/Arrays.java
@@ -1,9 +1,3 @@
-/*
-sumOfAnArray() - static method which sums up all the elements (integers) in an array
-maxValueInArray() - static method which returns the max value in an array of integers 
-isStringInArray() - static method which returns true if a string exists in an array, false otherwise
-Call each method 3 times from main (provide different arguments each time)
-*/
 //Name: Jaime Garcia Garcia
 //Date: 10-06-2018
 //Application: Arrays
