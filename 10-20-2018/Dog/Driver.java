f2b5641aad363e3253dc1cf24729a11f24259246
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-21 16:13:23 -0700

    comment each block of code with breed and line divider method

--- 10-20-2018/Dog/Driver.java
+++ 10-20-2018/Dog/Driver.java
@@ -1,3 +1,7 @@
+//Name: Jaime Garcia Garcia
+//Date: 10-06-2018
+//Application: Inheritance
+//Purpose: To learn about inheritance in the Java langauge
 /*
 -Extend this class 5 times, using a different dog breed (pick your favorite breeds)
 -Breed #1(Beagle): must use default name and override at least 1 other (different) feature/method
@@ -5,11 +9,24 @@
 -Breed #3(Dalmatian): must use default size and override at least 1 other (different) feature/method
 -Breed #4(Old English Sheepdog): must use default color and override at least 1 other (different) feature/method
 -Breed #5(Dutch Shepherd): must use default speed and override at least 1 other (different) feature/method
-
 */
 //Note refer to Inherentance
 class Driver {
+    public static void lineFormatter(){
+        int maxLine = 50;
+        int i = 0;
+        while(i <= maxLine){
+            if(i == maxLine){
+                System.out.print("\n");
+            }
+            else{
+                System.out.print("-");
+            }
+            i++;
+        }
+    }
     public static void main(String[] args) {
+        //Given Code
         String name = "Max";
         Lab myLab = new Lab(name);
         System.out.println("My dogs name is " + myLab.getName());
@@ -18,40 +35,51 @@ class Driver {
         System.out.println("The color of " + name + " is " + myLab.getColor());
         System.out.println("The size of " + name + " is " + myLab.getSize());
 
-        //My dog breeds
+        lineFormatter();
+
+        //Breed #1
         Beagle myBeagle = new Beagle();
         System.out.println("My dogs name is " + myBeagle.getName());
         System.out.println(myBeagle.getName() + " barks like this [" + myBeagle.bark() +"]");
         System.out.println(myBeagle.getName() + " has an average speed of: " + myBeagle.getSpeed());
         System.out.println("The color of " + myBeagle.getName() + " is " + myBeagle.getColor());
         System.out.println("The size of " + myBeagle.getName() + " is " + myBeagle.getSize());
+        lineFormatter();
 
+        //Breed #2
         Jindo myJindo = new Jindo();
         System.out.println("My dogs name is " + myJindo.getName());
         System.out.println(myJindo.getName() + " barks like this [" + myJindo.bark() +"]");
         System.out.println(myJindo.getName() + " has an average speed of: " + myJindo.getSpeed());
         System.out.println("The color of " + myJindo.getName() + " is " + myJindo.getColor());
         System.out.println("The size of " + myJindo.getName() + " is " + myJindo.getSize());
+        lineFormatter();
 
+        //Breed #3
         Dalmatian myDalmatian = new Dalmatian();
         System.out.println("My dogs name is " + myDalmatian.getName());
         System.out.println(myDalmatian.getName() + " barks like this [" + myDalmatian.bark() +"]");
         System.out.println(myDalmatian.getName() + " has an average speed of: " + myDalmatian.getSpeed());
         System.out.println("The color of " + myDalmatian.getName() + " is " + myDalmatian.getColor());
         System.out.println("The size of " + myDalmatian.getName() + " is " + myDalmatian.getSize());
+        lineFormatter();
 
+        //Breed #4
         OldEnglishSheepdog myOldEnglishSheepdog = new OldEnglishSheepdog();
         System.out.println("My dogs name is " + myOldEnglishSheepdog.getName());
         System.out.println(myOldEnglishSheepdog.getName() + " barks like this [" + myOldEnglishSheepdog.bark() +"]");
         System.out.println(myOldEnglishSheepdog.getName() + " has an average speed of: " + myOldEnglishSheepdog.getSpeed());
         System.out.println("The color of " + myOldEnglishSheepdog.getName() + " is " + myOldEnglishSheepdog.getColor());
         System.out.println("The size of " + myOldEnglishSheepdog.getName() + " is " + myOldEnglishSheepdog.getSize());
+        lineFormatter();
         
+        //Breed #5
         DutchShepherd myDutchShepherd = new DutchShepherd("Jocob");
         System.out.println("My dogs name is " + myDutchShepherd.getName());
         System.out.println(myDutchShepherd.getName() + " barks like this [" + myDutchShepherd.bark() +"]");
         System.out.println(myDutchShepherd.getName() + " has an average speed of: " + myDutchShepherd.getSpeed());
         System.out.println("The color of " + myDutchShepherd.getName() + " is " + myDutchShepherd.getColor());
         System.out.println("The size of " + myDutchShepherd.getName() + " is " + myDutchShepherd.getSize());
+        lineFormatter();
     }
 }
