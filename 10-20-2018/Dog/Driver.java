722f83e10440e9b98d5b8c82ea258951bab0d1c6
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-10-23 21:11:18 -0700

    remove the lab dog in Driver.java

--- 10-20-2018/Dog/Driver.java
+++ 10-20-2018/Dog/Driver.java
@@ -18,17 +18,6 @@ class Driver {
         }
     }
     public static void main(String[] args) {
-        //Given Code
-        String name = "Max";
-        Lab myLab = new Lab(name);
-        System.out.println("My dogs name is " + myLab.getName());
-        System.out.println(name + " barks like this [" + myLab.bark() +"]");
-        System.out.println(name + " has an average speed of: " + myLab.getSpeed());
-        System.out.println("The color of " + name + " is " + myLab.getColor());
-        System.out.println("The size of " + name + " is " + myLab.getSize());
-
-        lineDivider();
-
         //Breed #1
         Beagle myBeagle = new Beagle();
         System.out.println("My dogs name is " + myBeagle.getName());
