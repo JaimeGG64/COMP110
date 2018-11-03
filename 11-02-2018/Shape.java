473611a305dd58218183d35a2fc44b7b5d51231b
JaimeGG64 <jgarcia16432@gmail.com>
2018-11-02 21:32:16 -0700

    wrote some code for Shape.java

---
+++ 11-02-2018/Shape.java
@@ -0,0 +1,10 @@
+class Shape{
+    double width, height;
+    public setSize(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea(){
+        return width;
+    }
+}
