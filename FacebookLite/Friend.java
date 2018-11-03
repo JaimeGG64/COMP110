86c899a2a77ff47148309827340ae6124d1c1f89
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-11-03 13:02:15 -0700

    wrote lecture code for Facebook

---
+++ FacebookLite/Friend.java
@@ -0,0 +1,8 @@
+class Friend implements IDisplayable{
+    private Stack friends;
+    private boolean isFriendsVisible;
+    public Friend(){
+        friend = new Stack[5]; //5 for testing
+        isFriendsVisible = true;
+    }
+}
--- FacebookLite/User.java
+++ FacebookLite/User.java
@@ -1,12 +1,26 @@
-class User extends Person{
-    // @Override
-    public User(String name,String last, int age){
-        super(name,last,age);   
-    }
-
+class User extends Person implements IDisplayable{
+    /*
     public void printUserProfile(){
         String status = "Happy";
         System.out.println("Name:" + getName());
         System.out.println("Status:" + status);
+    }*/
+    private String status;
+    private boolean isAgeVisible;
+
+    public User(String name,String last, int age){
+        super(name,last,age);   
+        status = "what are you doing today";
+        isAgeVisiable = true;
+    }
+    public void display(){
+        System.out.println("Name:" + getName());
+        System.out.println("Status:" + status);
+        if(isAgeVisible){
+            System.out.print("Age:" + getAge());
+        }
+    }
+    public void toggleAgeVisible(){
+        isAgeVisible =! isAgeVisible;
     }
 }
