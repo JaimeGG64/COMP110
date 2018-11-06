2cf4ef40529c931c781eaef448c0e1ec11a9a97f
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-11-05 20:38:47 -0800

    translate prof. sudo code to java code

--- 11-03-2018/Polymorphism/Rectangle.java
+++ 11-03-2018/Polymorphism/Rectangle.java
@@ -3,3 +3,9 @@ Rectangle class inherits members of Shape class
 This class has only one method called “getArea”
 “getArea” method should return the area of a rectangle
 */
+public class Rectangle extends Shape{
+    @Override
+    public double getArea(){
+        return width*height;
+    }
+}
--- 11-03-2018/Polymorphism/Triangle.java
+++ 11-03-2018/Polymorphism/Triangle.java
@@ -3,3 +3,9 @@ Triangle class inherits members of Shape class
 This class has only one method called “getArea”
 “getArea” method should return the area of a triangle
 */
+public class Triangle extends Shape{
+    @Override
+    public double getArea(){
+        return (width*height)/2;
+    }
+}
