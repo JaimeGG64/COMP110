7a015fa0abacb6ccb06554578dacfdaf2dd2b5d4
JaimeGG64 <jgarcia16432@gmail.com>
2018-11-06 19:12:28 -0800

    got selection for FB Lite to work

--- FacebookLite/FacebookLite.java
+++ FacebookLite/FacebookLite.java
@@ -1,85 +1,49 @@
 import java.util.Scanner;
 class FacebookLite{
     public static void main(String[] args){
-        /*
-        Person person1 = new Person("Kenny", "McCormick",10);
-        Person person2 = new Person("Butter", "Stotch", 10);
-        User user1 = new User("Randy", "March", 42);
-        person1.printProfile();
-        person2.printProfile();
-        user1.printUserProfile();
-        System.out.println(person1.getName());
-        */
-        // private Scanner sc;
         
-        private Profile[] profiles;
-        private int idx;//index for profiles array
-        private int nop;//number of profiles
-        private int opt;//option
-
-        public void FacebookLite(){
-            profile = new Profile[5];
-            sc = new Scanner(System.in);
-            idx = -1;
-            nop = 0;
-        }
-
-        public createProfile(String name, String last, int age){
-            if(idx < profiles.length - 1){
-                Profile profile = new Profile(name,last,age);
-                idx ++;
-                profiles[idx] = profile;
-                nop++;
-            }
-            else{
-                Util.print("No room to create profile");
-            }
-        }
-
-        FacebookLite fbl = new FacebookLite();
-
-        while(true){
-            printMenu();
-            opt = Integer.parseInt(sc.nextLine());
-            switch(opt){
-                /* options
-                 0 - exit
-                 1 - create profile
-                 2 - delete last profile
-                 3 - switch profile
-                 4 - print profile
-                 5 - add friend
-                 6 - remove last friends
-                 7 - remove all friends
-                 8 - add post
-                 9 - remove last post
-                 10 - remove all post
-                 11 - toggle age
-                 12 - toggle friends
-                 13 - toggle post
-                 14 - change status
-                 15 - delete all profiles
-                */
+        Scanner userInput = new Scanner(System.in);
+        /* options
+            0 - exit
+            1 - create profile
+            2 - delete last profile
+            3 - switch profile
+            4 - print profile
+            5 - add friend
+            6 - remove last friends
+            7 - remove all friends
+            8 - add post
+            9 - remove last post
+            10 - remove all post
+            11 - toggle age
+            12 - toggle friends
+            13 - toggle post
+            14 - change status
+            15 - delete all profiles
+        */
+        int optionSelection = Integer.parseInt(userInput.nextLine());
+        while(optionSelection != 0){
+            // printMenu();
+            switch(optionSelection){
                 case 0:
-                    Util.print("Bye!");
+                    System.out.println("Bye!");
+                    optionSelection = Integer.parseInt(userInput.nextLine());
                     return;
                 case 1:
-                    System.out.print("Please enter name");
-                    String name = sc.nextLine();
-                    System.out.print("Please enter last name");
-                    String last = sc.nextLine();
-                    int age = Integer.parseInt(sc.nextLine());
-                    fbl.createProfile(name, last, age);
+                    System.out.println("Create Profile");
+                    optionSelection = Integer.parseInt(userInput.nextLine());
                     break;
                 case 2:
-                    if(fbl.nop == 0){
-                        Util.print("Create profile first");
-                    }
-                    else{
-                        fbl.removeLastProfile();
-                    }
+                    System.out.println("Wher is your profile");
+                    optionSelection = Integer.parseInt(userInput.nextLine());
+                    break;
+                default:
+                    System.out.println("WTF");
+                    optionSelection = Integer.parseInt(userInput.nextLine());
                     break;
             }
+            // int optionSelection = Integer.parseInt(sc.nextLine());
         }
+        userInput.close();
     }
 }
