4c4f377c40e10a2251b6a8f7b8398735b106060b
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-11-10 13:34:12 -0800

    got a checker pattern for the output

--- 11-10-2018/CheckersBoard/CheckersBoard.java
+++ 11-10-2018/CheckersBoard/CheckersBoard.java
@@ -4,14 +4,20 @@ class CheckersBoard{
         final int BLANK = 0;
         final int WHITE = 1;
         final int BLUE = 2;
-        final int SIZE = 5;
+        final int SIZE = 6;
 
         int[][] checkerboard = new int[SIZE][SIZE];
 
         for(int row = 0; row < checkerboard.length; row++){
             for(int col = 0; col < checkerboard.length; col++){
                 checkerboard[row][col] = row + col;
-                System.out.print(checkerboard[row][col] + "  ");
+                if(checkerboard[row][col] % 2 == 0){
+                    System.out.print(WHITE + " ");
+                }
+                else if(checkerboard[row][col] != 0){
+                    System.out.print(BLANK + " ");
+                }
+                // System.out.print(checkerboard[row][col] + "  ");
             }
             System.out.println();
         }
