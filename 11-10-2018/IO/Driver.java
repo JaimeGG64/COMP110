f818b5575a768854ef137b92666caf94d02723a0
JaimeGG64 <jgarcia16432@gmail.com>
2018-11-23 20:53:50 -0800

    made the Driver IO class generate a txt file

--- 11-10-2018/IO/Driver.java
+++ 11-10-2018/IO/Driver.java
@@ -7,14 +7,21 @@
         io.write();
     }
 */
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 class Driver{
     public static void main(String[] args){
-        int myIntArr[] = {23, 232, 342, 2, 535};
-        char ASCIICH = 121;
-        System.out.println("Hi");
-        System.out.println(ASCIICH);
-        Sort mySort = new Sort();
-        Sort.bubbleSort(myIntArr);
-        Sort.printArray(myIntArr);
+        File myFile = new File("test.txt");
+
+        try{
+            PrintWriter myOutput = new PrintWriter(myFile);
+            myOutput.println("John Doe");
+            myOutput.println(24);
+        }
+        catch(IOException ex){
+            System.out.print("Bad");
+        }
     }
 }
