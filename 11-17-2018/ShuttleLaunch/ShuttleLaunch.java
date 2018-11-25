713ddcff3b7da4c5881b5bbb0b1029f337a0674b
JaimeGG64 <jgarcia16432@gmail.com>
2018-11-24 22:43:12 -0800

    got ShuttleLaunch.java to count down

--- 11-17-2018/ShuttleLaunch/ShuttleLaunch.java
+++ 11-17-2018/ShuttleLaunch/ShuttleLaunch.java
@@ -2,11 +2,36 @@ class ShuttleLaunch{
     final static int SLEEP_TIME_MILLISECONDS = 1000;
     public static void main(String[] args) {
         String shuttleLaunchMessage = "Hello!\nWelcome to the Shuttle Launch!\n";
-        int[] countDown = new int[10];
+        int lengthOfCountDown = 11;
+        String[] countDownMessage = new String[lengthOfCountDown];
+        for(int i = 0; i < countDownMessage.length; i++){
+            countDownMessage[i] = "T - " + Integer.toString(i);
+        }
         for(int i = 0; i < shuttleLaunchMessage.length(); i++){
             try{
                 System.out.print(shuttleLaunchMessage.charAt(i));
                 Thread.sleep(SLEEP_TIME_MILLISECONDS/10);
+            }
+            catch(InterruptedException e){
+                System.out.print("Interrupted Exception");
+            }
+        }
+        for(int i = lengthOfCountDown - 1; i >= 0; i--){
+            try{
+                if(i == 2){
+                    System.out.println(countDownMessage[i]);
+                    System.out.println("Ignition...");
+                    Thread.sleep(SLEEP_TIME_MILLISECONDS);
+                }
+                else if(i == 0){
+                    System.out.println(countDownMessage[i]);
+                    System.out.println("We have a lifeoff");
+                    Thread.sleep(SLEEP_TIME_MILLISECONDS);
+                }
+                else{
+                    System.out.println(countDownMessage[i]);
+                    Thread.sleep(SLEEP_TIME_MILLISECONDS);
+                }
             }
             catch(InterruptedException e){
                 System.out.print("Interrupted Exception");
