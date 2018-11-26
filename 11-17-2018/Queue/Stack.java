ec2a88ab9de182ea1175f9a0895500efc0af29f8
JaimeGG64 <jgarcia16432@gmail.com>
2018-11-25 22:27:51 -0800

    copy same code from FacebookLite for Queue's Stack.java

---
+++ 11-17-2018/Queue/Stack.java
@@ -0,0 +1,54 @@
+public class Stack{
+    private int size;
+    private int top; //master index
+    private String[] stack;
+
+    public Stack(int size){
+        this.size = size;
+        stack = new String[size];
+        top = -1;
+    }
+    public int getSize(){
+        return size;
+    }
+
+    //Checks for status of the array
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+    public boolean isFull(){
+        return (top == size - 1);
+    }
+
+    public void push(String item){
+        if(isFull()){
+            System.out.println("Is Full");
+        }
+        else{
+            stack[++top] = item;
+        }
+    }
+
+    public String pop(){
+        if(isEmpty()){
+            System.out.println("It Empty");
+            return "";
+        }
+        else{
+            return stack[top--];
+        }        
+    }
+
+    public void reset(){
+        for(int i = 0; i < getSize(); i++){
+            stack[i] = "";
+        }
+        top =- 1;
+    }
+
+    public void print(){
+        for(int i = 0; i < getSize(); i++){
+            System.out.println(stack[i]);
+        }
+    }
+}
