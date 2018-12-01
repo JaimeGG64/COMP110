591f6f2f899d01fa615e7a31cf7ad1117e29ba5e
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-12-01 11:28:01 -0800

    add menu bar to GUI app

--- 12-01-2018/MyFirstGUIApp/MyFirstGUIApp.java
+++ 12-01-2018/MyFirstGUIApp/MyFirstGUIApp.java
@@ -1,18 +1,38 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+//Menu Bar
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 public class MyFirstGUIApp{
     //constructor
     public MyFirstGUIApp(){
         initComponents();
     }
     private void initComponents(){
+        //give title to GUI window
         JFrame frame = new JFrame("GUI App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //add labe to GUI
         JLabel label = new JLabel("Hello World");
         frame.getContentPane().add(label);
 
+        //build menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu helpMenu = new JMenu("Help");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        JMenuItem aboutItem = new JMenuItem("About");
+        fileMenu.add(exitItem);
+        helpMenu.add(aboutItem);
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+        
+        //Show GUI
+        frame.setJMenuBar(menuBar);
         frame.pack();
         frame.setVisible(true);
     }
