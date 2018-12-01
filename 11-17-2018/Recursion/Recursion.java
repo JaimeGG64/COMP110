60f72daebad36f4ae981189164c41983e6bfc0eb
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-12-01 10:27:46 -0800

    add messages to Recursion.java

--- 11-17-2018/Recursion/Recursion.java
+++ 11-17-2018/Recursion/Recursion.java
@@ -1,11 +1,12 @@
 
 class Recursion{
-    static void preDecrementMethod(int n){
-        if(1 == n){
-            return;
+    public static int simpleRecursion(int n) {    
+        if(1 == n) {
+            return n;
         }
-        preDecrementMethod(--n);
+        return simpleRecursion(n-1);
     }
+    
     public static int factorial(int n){
         if(n <= 1){
             return 1;
@@ -19,11 +20,23 @@ class Recursion{
         return fib(n - 1) + fib(n - 2);
     }
     public static void main(String[] args){
-        int test = 20;
-        fib(20);
-        System.out.println(factorial(20));
+        int simpleRecursionTest1 = simpleRecursion(20);
+        int simpleRecursionTest2 = simpleRecursion(7);
+        int simpleRecursionTest3 = simpleRecursion(4);
+
+        System.out.println("Simple Recursion method test: " + simpleRecursionTest1 + " " + simpleRecursionTest2 + " " + simpleRecursionTest3);
         
-        // System.out.print(test);
+        int factorialTest1 = factorial(20);
+        int factorialTest2 = factorial(7);
+        int factorialTest3 = factorial(-2);
+
+        System.out.println("Factorial method test: " + factorialTest1 + " " + factorialTest2 + " " + factorialTest3);
+        
+        int fibTest1 = fib(20);
+        int fibTest2 = fib(7);
+        int fibTest3 = fib(-2);
+
+        System.out.println("Fibonacci method test: " + fibTest1 + " " + fibTest2 + " " + fibTest3);
     }
     
 }
